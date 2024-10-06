9e3f3b7c09bf509b07f1eb402f7d9905805a119d
FireStyLe <szwx93@gmail.com>
2024-10-06 08:59:45 +0300

    feat: first commit

---
+++ appointFlow/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/home">Home</RouterLink>
+        <RouterLink to="/records">Records</RouterLink>
+      </nav>
+      <router-view/>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
